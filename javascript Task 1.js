055757c58d49681ab7a6962a1de09b4640ae4975
Coder22 <62084947+chiiedi@users.noreply.github.com>
2020-03-27 15:49:10 +0100

    Add files via upload

---
+++ javascript Task 1.js
@@ -0,0 +1,17 @@
+let name = "Chidi";
+
+console.log("My name is:"+name)
+
+ const data = [ "html", "CSS", "Javascript", "Coding"];
+
+ for (let i=0; i<=200; i++) {
+
+   if (i%2===0) {
+
+     data.push(i);
+
+   }
+
+ }
+
+ console.log(data);
