4bf7c23158f86256c4cd3416d041effe602a81bc
Coder22 <62084947+chiiedi@users.noreply.github.com>
2020-04-20 23:28:55 +0100

    Add files via upload

---
+++ JavaScript task 3.js
@@ -0,0 +1,10 @@
+function check_divisibility(10){
+    var output= [1,"yu","gi",4,"oh","yu-gi",7,"yu","gi","yu-oh"];
+    for(var i=0; i<=10; i++){
+    if(number%2===0){
+     return (output.push(i));
+    }
+    }
+    }
+    console.log(check_divisibility(10));
+   
